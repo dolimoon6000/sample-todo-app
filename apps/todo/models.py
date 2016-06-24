f17a669b2d1a7f8c5a4a2d750e82e0866fbca87c
Olu Soboyede <dolapo@yinkos.com>
2016-06-24 01:44:11 +0100

    Added model for todo items

--- apps/todo/models.py
+++ apps/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100)
+    position = models.IntegerField()
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('position', '-created_at')
