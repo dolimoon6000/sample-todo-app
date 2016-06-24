166d175899c69ee28ff3ea748106695a3dad62e4
Olu Soboyede <dolapo@yinkos.com>
2016-06-24 02:46:40 +0100

    Updated api url format and added api detail url

--- apps/todo/urls.py
+++ apps/todo/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^api/todoitem/$', views.todo_item_list),
+    url(r'^api/todoitem/(?P<pk>[0-9]+)/$', views.todo_item_detail),
 ]
